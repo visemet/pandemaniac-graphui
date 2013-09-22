a9f151d922a64ba2d5eed2f188f6866da900753f
Max Hirschhorn <maxh@caltech.edu>
2013-09-22 12:43:48 -0700

    Added support for additional playback controls.
    Includes step-forward, step-backward, and play. Note that a seek can be
    performed by changing the step number in the text field.
    
    As of right now, the entire simulation is reapplied, up to that point,
    when a step backwards is made; however a "reverse diff" could be
    constructed in an online fashion for efficiency.

--- public/js/render-graph.js
+++ public/js/render-graph.js
@@ -6,11 +6,22 @@ $(function() {
   $.getJSON('/api/v1/graph/' + id + '/structure', function(adj) {
     var prefix = '_';
 
-    var refs = {}
-      , nodes = $.map(adj, function(value, key) {
+    function computeNodes(refs) {
+      return $.map(adj, function(value, key) {
+        var node = refs[key];
+
+        if (node) {
+          delete node.team;
+        } else {
           refs[key] = { id: key };
-          return refs[key];
-        })
+        }
+
+        return refs[key];
+      });
+    };
+
+    var refs = {}
+      , nodes = computeNodes(refs)
       , links = d3.merge(nodes.map(function(source) {
           return adj[source.id].map(function(targetId) {
             return { source: refs[source.id]
@@ -38,9 +49,8 @@ $(function() {
         .data(nodes)
         .enter().append('svg:circle')
         .attr('r', 5)
-        .style('fill', 'gray')
-        .attr('class', 'node')
         .attr('id', function(d, i) { return prefix + d.id; })
+        .attr('class', 'node')
         .call(force.drag);
 
     var link = vis.selectAll('line.link')
@@ -58,42 +68,107 @@ $(function() {
           .attr('cy', function(d) { return d.y; });
     });
 
-    // TODO: get diff from the server
-    var steps = {
-      0: { 1: [ 1, 2, 3 ], 2: [ 4, 5, 6 ] }
-    , 1: { 1: [ 7, 8, 9 ], 2: [ 10, 11, 12 ] }
-    , 2: { 1: [ 13, 14, 15, 20, 21, 22 ], 2: [ 16, 17, 18, 19, 23, 24 ] }
+    function reset() {
+      node.style('fill', 'gray');
     };
 
-    var simulation = null;
+    reset();
 
-    var button = $('#chart').find('.btn').get(0);
-    button.onclick = function() {
-      var stepNo = 0;
-
-      // Prevent simultaneous updates
-      if (simulation) {
-        clearInterval(simulation);
-      }
-
-      node.style('fill', function(d) { return 'gray' });
-
-      simulation = setInterval(function() {
-        // Check when reached end of simulation
-        if (!steps.hasOwnProperty(stepNo)) {
-          return clearInterval(simulation);
-        }
-
-        $.each(steps[stepNo], function(key, values) {
+    function applyDiff(refs, diff) {
+      if (diff) {
+        $.each(diff, function(key, values) {
           $.each(values, function(i, value) {
-            node.filter('#' + prefix + value).style('fill', function(d) {
-              return color(key);
-            });
+            refs[value].team = key;
           });
         });
-
-        stepNo++;
-      }, 1000);
+      }
     };
+
+    // TODO: get diff from the server
+    var steps = {
+      0: { 1: [ '1', '2', '3' ], 2: [ '4', '5', '6' ] }
+    , 1: { 1: [ '7', '8', '9' ], 2: [ '10', '11', '12' ] }
+    , 2: { 1: [ '13', '14', '15', '20', '21', '22' ]
+         , 2: [ '16', '17', '18', '19', '23', '24' ]
+         }
+    };
+
+    var applied = -1;
+
+    function step(size) {
+      var stepNo = +$('#step-no').val()
+        , remain;
+
+      // Starting from step that was already applied,
+      // ...or a decrement
+      if (stepNo < applied || size < 0) {
+        computeNodes(refs);
+        remain = stepNo + size;
+        applied = -1;
+        stepNo = 0;
+      }
+
+      // Otherwise, an increment
+      else {
+        remain = size;
+      }
+
+      while (remain > 0) {
+        applyDiff(refs, steps[stepNo]);
+        applied++;
+        stepNo++;
+        remain--;
+      }
+
+      node.data(nodes).style('fill', function(d) {
+        if (d.team) {
+          return color(d.team);
+        }
+
+        return 'gray';
+      });
+
+      $('#step-no').val(stepNo);
+    };
+
+    function run(delay) {
+      var simulation = null;
+
+      $('#play').click(function() {
+        // Prevent simultaneous updates
+        if (simulation) {
+          clearInterval(simulation);
+        }
+
+        simulation = setInterval(function() {
+          var stepNo = +$('#step-no').val();
+
+          // Check when reached end of simulation
+          if (!steps.hasOwnProperty(stepNo)) {
+            return clearInterval(simulation);
+          }
+
+          step(1);
+        }, delay);
+      });
+
+      $('#step-forward').click(function() {
+        if (simulation) {
+          clearInterval(simulation);
+        }
+
+        step(1);
+      });
+
+      $('#step-backward').click(function() {
+        if (simulation) {
+          clearInterval(simulation);
+        }
+
+        step(-1);
+      });
+    };
+
+    run(1000);
   });
 });
