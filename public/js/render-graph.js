37fd4d0f36c584f0ba3b1709cfb1b72398ba778e
Max Hirschhorn <maxh@caltech.edu>
2013-11-03 11:38:14 -0800

    Refactored code to render the graph using d3.js.
    Now loads the graph in a static layout, per the new layout route, which
    specifies the (x, y)-coordinates for each node. This enables reasonable
    performance (and interactability) for larger graphs.

--- public/js/render-graph.js
+++ public/js/render-graph.js
@@ -4,165 +4,191 @@ $(function() {
 
   // Get adjacency list from the server
   $.getJSON('/api/v1/graph/' + id + '/structure', function(adj) {
-    var prefix = '_';
 
-    function computeNodes(refs) {
-      return $.map(adj, function(value, key) {
-        var node = refs[key];
+    // Get graph layout from the server
+    $.getJSON('/api/v1/graph/' + id + '/layout', function(pos) {
+      var prefix = '_';
 
-        if (node) {
-          delete node.team;
-        } else {
-          refs[key] = { id: key };
-        }
+      function computeNodes(refs) {
+        return $.map(adj, function(value, key) {
+          var node = refs[key];
 
-        return refs[key];
-      });
-    };
-
-    var refs = {}
-      , nodes = computeNodes(refs)
-      , links = d3.merge(nodes.map(function(source) {
-          return adj[source.id].map(function(targetId) {
-            return { source: refs[source.id]
-                   , target: refs[targetId]
-                   };
-          });
-        }));
-
-    var width = 960
-      , height = 500;
-
-    var color = d3.scale.category10();
-
-    var force = d3.layout.force()
-        .nodes(nodes)
-        .links(links)
-        .size([ width, height ])
-        .start();
-
-    var vis = d3.select('#chart').append('svg:svg')
-        .attr('width', width)
-        .attr('height', height);
-
-    var node = vis.selectAll('circle.node')
-        .data(nodes)
-        .enter().append('svg:circle')
-        .attr('r', 5)
-        .attr('id', function(d, i) { return prefix + d.id; })
-        .attr('class', 'node')
-        .call(force.drag);
-
-    var link = vis.selectAll('line.link')
-        .data(links)
-        .enter().append('svg:line')
-        .attr('class', 'link');
-
-    force.on('tick', function() {
-      link.attr('x1', function(d) { return d.source.x; })
-          .attr('y1', function(d) { return d.source.y; })
-          .attr('x2', function(d) { return d.target.x; })
-          .attr('y2', function(d) { return d.target.y; });
-
-      node.attr('cx', function(d) { return d.x; })
-          .attr('cy', function(d) { return d.y; });
-    });
-
-    function reset() {
-      node.style('fill', 'gray');
-    };
-
-    reset();
-
-    function applyDiff(refs, diff) {
-      if (diff) {
-        $.each(diff, function(key, values) {
-          $.each(values, function(i, value) {
-            refs[value].team = key;
-          });
-        });
-      }
-    };
-
-    // Get steps of simulation from the server
-    $.getJSON('/api/v1/graph/' + id + '/model', function(steps) {
-      var applied = -1;
-
-      function step(size) {
-        var stepNo = +$('#step-no').val()
-          , remain;
-
-        // Starting from step that was already applied,
-        // ...or a decrement
-        if (stepNo < applied || size < 0) {
-          computeNodes(refs);
-          remain = stepNo + size;
-          applied = -1;
-          stepNo = 0;
-        }
-
-        // Otherwise, an increment
-        else {
-          remain = size;
-        }
-
-        while (remain > 0) {
-          applyDiff(refs, steps[stepNo]);
-          applied++;
-          stepNo++;
-          remain--;
-        }
-
-        node.data(nodes).style('fill', function(d) {
-          if (d.team) {
-            return color(d.team);
+          if (node) {
+            delete node.team;
+          } else {
+            refs[key] = { id: key, x: pos[key].x, y: pos[key].y };
           }
 
-          return 'gray';
+          return refs[key];
         });
-
-        $('#step-no').val(stepNo);
       };
 
-      function run(delay) {
-        var simulation = null;
+      var refs = {}
+        , nodes = computeNodes(refs)
+        , links = d3.merge(nodes.map(function(source) {
+            return adj[source.id].map(function(targetId) {
+              return { source: refs[source.id]
+                     , target: refs[targetId]
+                     };
+            });
+          }));
 
-        $('#play').click(function() {
-          // Prevent simultaneous updates
-          if (simulation) {
-            clearInterval(simulation);
+      function zoom() {
+        vis.attr('transform', 'translate(' + d3.event.translate + ')'
+            + 'scale(' + d3.event.scale + ')');
+      };
+
+      var width = 960
+        , height = 500;
+
+      var color = d3.scale.category10();
+
+      var force = d3.layout.force()
+          .nodes(nodes)
+          .links(links)
+          .size([ width, height ]);
+
+      var vis = d3.select('#chart')
+          .append('svg:svg')
+            .attr('width', width)
+            .attr('height', height)
+            .attr('pointer-events', 'all')
+          .append('svg:g')
+            .attr('width', width)
+            .attr('height', height)
+            .call(d3.behavior.zoom().scaleExtent([0.25, 4]).on('zoom', zoom))
+          .append('svg:g')
+            .attr('width', width)
+            .attr('height', height);
+
+      vis.append('svg:rect')
+          .attr('width', width)
+          .attr('height', height)
+          .style('fill', 'white');
+
+      var link = vis.selectAll('line.link')
+          .data(links)
+          .enter().append('svg:line')
+          .attr('class', 'link')
+          .attr('x1', function(d) { return d.source.x; })
+          .attr('y1', function(d) { return d.source.y; })
+          .attr('x2', function(d) { return d.target.x; })
+          .attr('y2', function(d) { return d.target.y; })
+          .style('stroke', 'gray')
+          .style('opacity', 0.1);
+
+      var node = vis.selectAll('circle.node')
+          .data(nodes)
+          .enter().append('svg:circle')
+          .attr('r', 3)
+          .attr('transform', function(d) { return 'translate(' + d + ')'; })
+          .attr('id', function(d, i) { return prefix + d.id; })
+          .attr('class', 'node')
+          .attr('cx', function(d) { return d.x; })
+          .attr('cy', function(d) { return d.y; })
+          .style('fill', 'gray')
+          .style('opacity', 0.8);
+
+      var colors = {}
+        , num_colors = 0;
+
+      function applyDiff(refs, diff) {
+        if (diff) {
+          $.each(diff, function(key, values) {
+            $.each(values, function(i, value) {
+              refs[value].team = colors[key];
+            });
+          });
+        }
+      };
+
+      // Get steps of simulation from the server
+      $.getJSON('/api/v1/graph/' + id + '/model', function(steps) {
+        $.each(steps['0'], function(key, values) {
+          num_colors++;
+          colors[key] = num_colors;
+        });
+
+        var applied = -1;
+
+        function step(size) {
+          var stepNo = +$('#step-no').val()
+            , remain;
+
+          // Starting from step that was already applied,
+          // ...or a decrement
+          if (stepNo < applied || size < 0) {
+            computeNodes(refs);
+            remain = stepNo + size;
+            applied = -1;
+            stepNo = 0;
           }
 
-          simulation = setInterval(function() {
-            var stepNo = +$('#step-no').val();
+          // Otherwise, an increment
+          else {
+            remain = size;
+          }
 
-            // Check when reached end of simulation
-            if (!steps.hasOwnProperty(stepNo)) {
-              return clearInterval(simulation);
+          var changes = [];
+
+          while (remain > 0) {
+            applyDiff(refs, steps[stepNo]);
+            applied++;
+            stepNo++;
+            remain--;
+          }
+
+          node.data(nodes).style('fill', function(d) {
+              if (d.team) {
+                return color(d.team);
+              }
+
+              return 'gray';
+            });
+
+          $('#step-no').val(stepNo);
+        };
+
+        function run(delay) {
+          var simulation = null;
+
+          $('#play').click(function() {
+            // Prevent simultaneous updates
+            if (simulation) {
+              clearInterval(simulation);
+            }
+
+            simulation = setInterval(function() {
+              var stepNo = +$('#step-no').val();
+
+              // Check when reached end of simulation
+              if (!steps.hasOwnProperty(stepNo)) {
+                return clearInterval(simulation);
+              }
+
+              step(1);
+            }, delay);
+          });
+
+          $('#step-forward').click(function() {
+            if (simulation) {
+              clearInterval(simulation);
             }
 
             step(1);
-          }, delay);
-        });
+          });
 
-        $('#step-forward').click(function() {
-          if (simulation) {
-            clearInterval(simulation);
-          }
+          $('#step-backward').click(function() {
+            if (simulation) {
+              clearInterval(simulation);
+            }
 
-          step(1);
-        });
+            step(-1);
+          });
+        };
 
-        $('#step-backward').click(function() {
-          if (simulation) {
-            clearInterval(simulation);
-          }
-
-          step(-1);
-        });
-      };
-
-      run(1000);
+        run(1000);
+      });
     });
   });
 });
