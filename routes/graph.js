771c5dfe79657e0e6435e81fecb049b90f4574d2
Max Hirschhorn <maxh@caltech.edu>
2013-12-05 20:42:13 -0800

    Created dashboard view for submissions.
    Displays all runs in a table with teams along the rows and graphs
    across the columns. If the team has played on a particular graph, then
    a link will be present that shows the cascade.

--- routes/graph.js
+++ routes/graph.js
@@ -17,6 +17,121 @@ exports.index = function(req, res, next) {
   res.render('graph/graph');
 };
 
+function findAllTeams(teams, next) {
+  var res = [];
+
+  teams.find({}, { sort: 'name' }).each(function(err, doc) {
+    if (err) {
+      return next(err);
+    }
+
+    // Check if have exhausted cursor
+    if (doc === null) {
+      return next(null, res);
+    }
+
+    res.push(doc.name);
+  });
+};
+
+function findAllGraphs(graphs, next) {
+  var res = [];
+
+  // TODO: group by category
+  graphs.find({}, { sort: 'name' }).each(function(err, doc) {
+    if (err) {
+      return next(err);
+    }
+
+    // Check if have exhausted cursor
+    if (doc === null) {
+      return next(null, res);
+    }
+
+    res.push(doc.name);
+  });
+};
+
+function findAllRuns(runs, next) {
+  runs.aggregate([{ $unwind: '$teams' }], next);
+};
+
+/*
+ * GET listing of simulations.
+ */
+exports.list = function(req, res, next) {
+  MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
+    if (err) {
+      return next(err);
+    }
+
+    var teams = db.collection('teams')
+      , graphs = db.collection('graphs')
+      , runs = db.collection('runs');
+
+    function doNext(err) {
+      db.close();
+      next(err);
+    };
+
+    // Get all teams
+    findAllTeams(teams, function(err, teams) {
+      if (err) {
+        return doNext(err);
+      }
+
+      // Get all graphs, grouped by category
+      findAllGraphs(graphs, function(err, graphs) {
+        if (err) {
+          doNext(err);
+        }
+
+        // Iterate through all runs, unwound by team
+        findAllRuns(runs, function(err, runs) {
+          if (err) {
+            doNext(err);
+          }
+
+          var invTeams = {};
+          teams.forEach(function(value, index) {
+            invTeams[value] = index;
+          });
+
+          var invGraphs = {};
+          graphs.forEach(function(value, index) {
+            invGraphs[value] = index;
+          });
+
+          var matrix = [];
+
+          // Initialize empty matrix
+          // number of teams by number of graphs
+          teams.forEach(function() {
+            var row = [];
+
+            graphs.forEach(function() {
+              row.push(null);
+            });
+
+            matrix.push(row);
+          });
+
+          runs.forEach(function(doc) {
+            var i = invTeams[doc.teams];
+            var j = invGraphs[doc.graph];
+
+            matrix[i][j] = doc._id;
+          });
+
+          res.render('graph/dashboard', { matrix: matrix
+                                        , teams: teams
+                                        , graphs: graphs });
+        });
+      });
+    });
+  });
+};
+
 /*
  * GET structure of a simulation.
  */
