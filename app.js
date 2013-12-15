4af798fa517c2f31883e70982b916fb6e19dccdd
Max Hirschhorn <maxh@caltech.edu>
2013-12-15 11:24:47 -0800

    Added route to list all simulations in table form.

--- app.js
+++ app.js
@@ -144,6 +144,7 @@ app.get('/submit/:id', restrict, submit.index);
 app.get('/submit/:id/download', restrict, submit.download);
 app.post('/submit/:id/upload', restrict, submit.upload);
 
+app.get('/graph', graph.list);
 app.get('/graph/:id', graph.index);
 app.get('/api/v1/graph/:id/structure', graph.structure);
 app.get('/api/v1/graph/:id/layout', graph.layout);
